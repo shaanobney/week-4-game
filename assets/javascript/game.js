881a0604be0b42562921288fc091e4bf17889943
shaanobney <shaan.obney@gmail.com>
2016-07-27 16:11:15 -0700

    threw out old idea

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,102 +1,175 @@
 $(document).ready(function() {
-  	$('#elaine').mousedown(function() {
-		$('#george').appendTo("#defenderSelect").css("background-color","red");
-		$('#jerry').appendTo("#defenderSelect").css("background-color","red");
-		$('#kramer').appendTo("#defenderSelect").css("background-color","red");
-		elaineWav.play();
-	});
-	$('#george').mousedown(function() {
-		$('#elaine').appendTo("#defenderSelect").css("background-color","red");
-		$('#jerry').appendTo("#defenderSelect").css("background-color","red");
-		$('#kramer').appendTo("#defenderSelect").css("background-color","red");
-		georgeWav.play();	
-	});
-	$('#jerry').mousedown(function() {
-		$('#kramer').appendTo("#defenderSelect").css("background-color","red");
-		$('#elaine').appendTo("#defenderSelect").css("background-color","red");
-		$('#george').appendTo("#defenderSelect").css("background-color","red");
-		jerryWav.play();
-	});
-	$('#kramer').mousedown(function() {
-		$('#jerry').appendTo("#defenderSelect").css("background-color","red");
-		$('#george').appendTo("#defenderSelect").css("background-color","red");
-		$('#elaine').appendTo("#defenderSelect").css("background-color","red");
-		kramerWav.play();
-	});
-});
-
-//VARIABLES
-
-var seinFeld = ["#Elaine", "#George", "#Jerry", "#Kramer"];
-var elaineWav = new Audio('dingo.wav');
-var georgeWav = new Audio('ah_hah.wav');
-var jerryWav = new Audio('dream_hamburger.wav')
-var kramerWav = new Audio('assman.wav')
-
 
 //OBJECTS. Properties assigned to hitpoints, attack power, base to add back to attack.
+    var characters = {
+        elaine: {
+            hp: 200,
+            attack: 10,
+            name: 'Elaine',
+            imgSrc: "assets/images/elaine.jpg",
+            audio: elaineWav,
 
-var elaine = {
-	hitPoints: 80,
-	attackPower: 40,
-	base: 3,
-	name: "Elaine",
-	card: '#elaine',
-}
+        },
+        george: {
+            hp: 100,
+            attack: 8,
+            name: 'George',
+            imgSrc: "assets/images/george.jpg",
+            audio: georgeWav,
+        },
+        jerry: {
+            hp: 150,
+            attack: 12,
+            name: ' Jerry ',
+            imgSrc: "assets/images/jerry.jpg",
+            audio: jerryWav,
+        },
+        kramer: {
+            hp: 300,
+            attack: 2,
+            name: 'Kramer',
+            imgSrc: "assets/images/kramer.jpg",
+            audio: kramerWav,
+        }
+    };
+//VARIABLES.
+    var charElement = '';
+    var opponent = '';
+    var myCharToggle = false;
+    var opponentToggle = false;
+    var charSel = 0;
+    var myHp = 0;
+    var myWav = "";
+    var opponentHp = '';
+    var enemsBeaten = 0;
+    var elaineWav = new Audio('dingo.wav');
+    var georgeWav = new Audio('ah_hah.wav');
+    var jerryWav = new Audio('dream_hamburger.wav');
+    var kramerWav = new Audio('assman.wav');
 
-var george = {
-	hitPoints: 30,
-	attackPower: 5,
-	base: 50,
-	name: "George",
-	card: '#george',
+    function setCond() {
+        // CREATES CHARACTERS, LOOPS THROUGH AND SPITS OUT BASED ON OBJECT PROPS.
+        for (var property in characters) {
+            var charStat = characters[property];
+            $('.characters').append("<div class='char' id='" + property + "'>" + charStat.name + "<img src='" + charStat.imgSrc + "'></div>");
+	    }
+    }
 
-}
+    setCond();
 
-var jerry = {
-	hitPoints: 120,
-	attackPower: 50,
-	base: 10,
-	name: "Jerry",
-	card: '#jerry',
-}
+    $('body').on('click', '.char', function(e) {
+        e.preventDefault();
+        e.stopPropagation();
+        charElement = $(this).attr('id')
 
-var kramer = {
-	hitPoints: 200,
-	attackPower: 10,
-	base: 1.2,
-	name: "Kramer",
-	card: '#kramer',
-}
+        if (opponentToggle === false) {
+            charSel = charElement;
+            $("#charSel").append($('#' + charSel));
+            myHp = characters[charSel].hp;
+            myWav = characters[charSel].audio;
+            $('#myHpValue').text(characters[charSel].hp);
+            opponentToggle = true
+        }
+ 
+        else {
+            if (opponent === '') {
+                opponent = charElement;
+                $("#opponent").append($('#' + opponent));
+                opponentHp = characters[opponent].hp;
+                $('#opponentHpValue').text(characters[opponent].hp);
+            }
+        }
+    })
 
-// //Fight function. Compares the attack power of each to the hit points of the other. if attack is greater than HP
-// //you're dead. Otherwise it subtracts attack power from hitpoints and updates running total for each char, and outputs msg.
+ //Fight function. Compares the attack power of each to the hit points of the other. if attack is greater than HP
+//you're dead. Otherwise it subtracts attack power from hitpoints and updates running total for each char, and outputs msg.
 
+    $('#attackbutton').on('click', function(attack) {
+        characters[charSel].hp = characters[charSel].hp - characters[opponent].attack;
+        characters[opponent].hp -= characters[charSel].attack;
+        characters[charSel].attack += 5;
+        var myCharHp = characters[charSel].hp;
+        var opponentCharHp = characters[opponent].hp;
+        if (myCharHp < 0) {
+            myCharHp = 0;
+        }
+        if (opponentCharHp < 0) {
+            opponentCharHp = 0;
+        }
+        $('#myHpValue').text(myCharHp);
+        $('#opponentHpValue').text(opponentCharHp);
 
+        if (characters[charSel].hp <= 0) {
+            document.getElementById('ticker').innerHTML = "Yada Yada... you died";
+        }
 
-// //need function for picking from enemies pool and sending it to defender. 
+        if (characters[opponent].hp <= 0) {
+            charElement = '';
+            $('#opponent').empty();
+            document.getElementById('ticker').innerHTML = "Yada Yada You win the battle";
+            opponentHp = '';
+            opponent = '';
+            enemsBeaten++;
+            
+        if (enemsBeaten < 3) {
+                document.getElementById('ticker').innerHTML = "Pick another clown";
+            };
+        }
 
-function compareChar(o1, o2) {
-		if (o2.hitPoints <= o1.attackPower) {
-			alert("Yada Yada " + o2.name + " died");
-			document.getElementById('charAttack').innerHTML = "Yada Yada " + o2.name + " died";
-		} else {
-			o2.hitPoints = (o2.hitPoints - o1.attackPower);
-			document.getElementById('charAttack').innerHTML = o1.name + " did " + o1.attackPower + " Yada Yadas (damage)";
-			o1.attackPower = (o1.attackPower + o1.base);
-		}
-		if (o1.hitPoints <= o2.attackPower) {
-			alert("I'll bet they'll replace me with Banya!");
-			document.getElementById('defAttack').innerHTML = "I'll bet they'll replace me with Banya!)";
-		} else {
-			o1.hitpoints = (o1.hitPoints - o2.attackPower);
-			document.getElementById('defAttack').innerHTML = o2.name + " did " + o2.attackPower + " Yada Yadas (damage)";
-			o2.attackPower = (o2.attackPower + o2.base);
-		}
-	};
+        if (enemsBeaten === 3) {
+            document.getElementById('ticker').innerHTML ="You Win";
+        }
 
-	compareChar();
+    })
+    // Resets game to initial conditions, clears out values. 
+    function reset() {
+        characters = {
+        elaine: {
+            hp: 200,
+            attack: 10,
+            name: 'Elaine',
+            imgSrc: "assets/images/elaine.jpg",
+            audio: elaineWav,
+        },
+        george: {
+            hp: 100,
+            attack: 8,
+            name: 'George',
+            imgSrc: "assets/images/george.jpg",
+            audio: georgeWav,
+        },
+        jerry: {
+            hp: 150,
+            attack: 12,
+            name: ' Jerry ',
+            imgSrc: "assets/images/jerry.jpg",
+            audio: jerryWav,
+        },
+        kramer: {
+            hp: 300,
+            attack: 2,
+            name: 'Kramer',
+            imgSrc: "assets/images/kramer.jpg",
+            audio: kramerWav,
+            }
+        };
 
+        charElement = '';
+        opponent = '';
+        opponentToggle = false;
+        charSel = '';
+        enemsBeaten = 0;
+        $('.characters').empty();
+        $('#charSel').empty();
+        $('#opponent').empty();
+        $('#myHpValue').empty();
+        $('#opponentHpValue').empty();
+        $('#ticker').empty();
+        setCond();
+    }
 
+    $('#resetbutton').on('click', function() {
+        reset();
 
-
+    })
+});
